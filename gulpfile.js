7ea5f6d9e289ed8e314239781ba8bc7e57ecea62
Jimi-Chhatrala <jimichhatrala8488@gmail.com>
2024-07-30 17:49:55 +0530

    Project Srtucture

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("mysass/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["mysass/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
